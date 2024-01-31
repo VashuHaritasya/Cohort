6db7c3ab16bf2aa779479e43d0381d881c310f74
Vashu Haritasya <vashusharma246@gmail.com>
2024-01-31 20:54:03 +0530

    Tailwind-Project

---
+++ tailwind-project/src/componets/RevenueCard.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+export const RevenueCard = ({ title, orderCount, amount }) => {
+  return (
+    <div className="bg-white rounded shadow-md p-4">
+      <div className="text-grey-700 flex justify-centre flex-col hover:bg-red-100">
+        <div className="flex">
+          <div>{title}</div>
+          <div className="ml-1 flex justify-centre flex-col">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-4 h-4"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z"
+              />
+            </svg>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-between pt-2">
+        <div className="font-semibold text-2xl">$ {amount}</div>
+        {orderCount ? (
+          <div className="flex cursor-pointer underline font-medium flex flex-col justify-centre">
+            <div className="flex">
+              <div className="text-blue-7">{orderCount} order(s)</div>
+              <svg
+                className="fill-blue-700"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-4 h-4"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="m8.25 4.5 7.5 7.5-7.5 7.5"
+                />
+              </svg>
+            </div>
+          </div>
+        ) : null}
+      </div>
+    </div>
+  );
+};
