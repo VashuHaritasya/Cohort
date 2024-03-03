55b976fe665d9a961131f72004f4126169acfbd8
Vashu Haritasya <vashusharma246@gmail.com>
2024-03-03 19:29:29 +0530

    Prisma. relationship in prisma

---
+++ Week-12-5.2/src/index.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from "@prisma/client";
+import { log } from "console";
+
+const prisma = new PrismaClient();
+
+async function insertUser(
+  username: string,
+  password: string,
+  firstName: string,
+  lastName: string
+) {
+  const response = await prisma.user.create({
+    data: {
+      username,
+      password,
+      firstName,
+      lastName,
+    },
+  });
+  console.log(response);
+}
+
+insertUser("test", "test", "test", "test");
