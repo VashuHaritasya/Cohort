2d09d3548f17512aed78159d960138cfffba5f33
Vashu Haritasya <vashusharma246@gmail.com>
2024-03-05 19:36:47 +0530

    Project-1

---
+++ medium/backend/src/index.ts
@@ -0,0 +1,49 @@
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { sign } from "hono/jwt";
+
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+  };
+}>();
+
+app.post("/api/v1/signup", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+
+  const user = await prisma.user.create({
+    data: {
+      email: body.email,
+      password: body.password,
+    },
+  });
+
+  const token = await sign({ id: user.id }, c.env.JWT_SECRET);
+
+  return c.json({
+    jwt: token,
+  });
+});
+
+app.post("/api/v1/signin", (c) => {
+  return c.text("signin route");
+});
+
+app.get("/api/v1/blog/:id", (c) => {
+  return c.text("get blog route");
+});
+
+app.post("/api/v1/blog", (c) => {
+  return c.text("signin route");
+});
+
+app.put("/api/v1/blog", (c) => {
+  return c.text("signin route");
+});
+
+export default app;
